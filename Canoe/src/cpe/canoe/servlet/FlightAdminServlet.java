f2be08ff1211f51086fac8ae84053cd25acdd5ce
remimarenco <remi.marenco@gmail.com>
2013-01-28 11:48:16 +0100

    Ajout de la servlet FlightAdmin

---
+++ Canoe/src/cpe/canoe/servlet/FlightAdminServlet.java
@@ -0,0 +1,27 @@
+package cpe.canoe.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FlightAdminServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		RequestDispatcher dispatcher = null;
+		
+		dispatcher = req.getRequestDispatcher("/flightAdmin.jsp");
+		
+		
+		
+		try {
+			dispatcher.forward(req, resp);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
